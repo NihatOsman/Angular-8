54ccfc5d89987f852e26543638786bd7923839ce
Nihat Osman <nihat.osman@elando.bg>
2019-10-11 15:50:19 +0300

    binding to custom Properties

--- Databinding/src/app/app.component.ts
+++ Databinding/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Databinding';
+  serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test!'}];
 }
