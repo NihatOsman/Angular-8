09292683e8d599a96918044a307a1a1f9eafe160
Nihat Osman <nihat.osman@elando.bg>
2019-10-14 15:19:21 +0300

    first commit

---
+++ Game/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onGameStarted(counter: number) {
+    if (counter % 2 === 0) {
+      this.evenNumbers.push(counter);
+    } else {
+      this.oddNumbers.push(counter);
+    }
+  }
+}
+
+
