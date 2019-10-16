357fdf1d6612ee2e64e8ba9602db4d672d5951eb
Nihat Osman <nihat.osman@elando.bg>
2019-10-16 17:31:29 +0300

    added Ingredients to Recipes

--- RecipeBook/src/app/recipes/recipe.service.ts
+++ RecipeBook/src/app/recipes/recipe.service.ts
@@ -1,13 +1,27 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>()
+  recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Another test Recipe', 'this is simply a test',
-      'https://static01.nyt.com/images/2017/09/27/dining/27KITCHENSAUSAGES1/27KITCHENSAUSAGES-articleLarge.jpg'),
-    new Recipe('A TEST Recipe', 'this is simply a test',
-      'https://static01.nyt.com/images/2017/09/27/dining/27KITCHENSAUSAGES1/27KITCHENSAUSAGES-articleLarge.jpg')
+    new Recipe('Another test Recipe',
+      'this is simply a test',
+      'https://static01.nyt.com/images/2017/09/27/dining/27KITCHENSAUSAGES1/27KITCHENSAUSAGES-articleLarge.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fries', 20)
+      ]
+    ),
+
+    new Recipe('A TEST Recipe',
+      'this is simply a test',
+      'https://static01.nyt.com/images/2017/09/27/dining/27KITCHENSAUSAGES1/27KITCHENSAUSAGES-articleLarge.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Fries', 10)
+      ]
+    )
   ];
 
   getRecipes() {
