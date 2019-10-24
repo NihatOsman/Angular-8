7a466cb44da38c0106f60875399aff9984d10924
Nihat Osman <nihat.osman@elando.bg>
2019-10-24 17:37:03 +0300

    task is done

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('signupForm', {static: false}) sgnForm: NgForm;
+  defaultQuestion = 'advanced';
+  user = {
+    email: '',
+    password: ''
+  };
+  subscription = '';
 
+  onSubmit() {
+    if (this.sgnForm.valid) {
+      this.user.email = this.sgnForm.value.email;
+      this.user.password = this.sgnForm.value.password;
+      this.subscription = this.sgnForm.value.subscription;
+      this.sgnForm.reset();
+    }
+  }
 }
