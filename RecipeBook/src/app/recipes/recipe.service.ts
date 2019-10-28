be5364769cb55dc32c922068e1ba432f89ce4ab7
Nihat Osman <nihat.osman@elando.bg>
2019-10-28 18:17:23 +0200

    Submitting the Recipe Edit Form

--- RecipeBook/src/app/recipes/recipe.service.ts
+++ RecipeBook/src/app/recipes/recipe.service.ts
@@ -2,10 +2,12 @@ import {Recipe} from './recipe.model';
 import {Injectable} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {Subject} from 'rxjs';
 
 @Injectable()
 export class RecipeService {
-  recipes: Recipe[] = [
+  recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [
     new Recipe('Another test Recipe',
       'this is simply a test',
       'https://static01.nyt.com/images/2017/09/27/dining/27KITCHENSAUSAGES1/27KITCHENSAUSAGES-articleLarge.jpg',
@@ -35,10 +37,19 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
-
   }
 
   getRecipe(index: number) {
     return this.recipes[index];
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
