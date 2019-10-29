30df86a5e89786eb935c1e77ae1d0d5e9dd44f2e
Nihat Osman <nihat.osman@elando.bg>
2019-10-29 10:51:13 +0200

    adding a Delete and Clear(Cancel) Functionality

--- RecipeBook/src/app/recipes/recipe.service.ts
+++ RecipeBook/src/app/recipes/recipe.service.ts
@@ -1,8 +1,8 @@
-import {Recipe} from './recipe.model';
-import {Injectable} from '@angular/core';
-import {Ingredient} from '../shared/ingredient.model';
-import {ShoppingListService} from '../shopping-list/shopping-list.service';
-import {Subject} from 'rxjs';
+import { Recipe } from './recipe.model';
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
@@ -50,6 +50,11 @@ export class RecipeService {
 
   updateRecipe(index: number, newRecipe: Recipe) {
     this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
     this.recipesChanged.next(this.recipes.slice());
   }
 }
