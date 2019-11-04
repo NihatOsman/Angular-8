a9d9bafa00b6f11303381b3fb77f14e773cb8860
Nihat Osman <nihat.osman@elando.bg>
2019-11-04 16:36:16 +0200

    Setting up The DataStorage Service and Storing Recipes

--- RecipeBook/src/app/header/header.component.ts
+++ RecipeBook/src/app/header/header.component.ts
@@ -1,12 +1,15 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  templateUrl: './header.component.html'
 })
-
 export class HeaderComponent {
   collapsed = true;
+  constructor(private dataStorageService: DataStorageService) {}
 
-
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
---
+++ RecipeBook/src/app/shared/data-storage.service.ts
@@ -0,0 +1,17 @@
+import { RecipeService } from './../recipes/recipe.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put('https://recipe-book-74c9c.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+}
