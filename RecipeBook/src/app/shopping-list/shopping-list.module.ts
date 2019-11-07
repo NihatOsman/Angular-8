e50cb3617d50044df51fbcb21e490d62a6fbe84a
Nihat Osman <nihat.osman@elando.bg>
2019-11-07 16:45:28 +0200

    Understanding the Core Module

--- RecipeBook/src/app/shopping-list/shopping-list.module.ts
+++ RecipeBook/src/app/shopping-list/shopping-list.module.ts
@@ -4,9 +4,10 @@ import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ShoppingListRoutingModule } from './shopping-list-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingEditComponent],
-  imports: [CommonModule, FormsModule, ShoppingListRoutingModule]
+  imports: [FormsModule, ShoppingListRoutingModule, SharedModule]
 })
 export class ShoppingListModule {}
