85646893774ffbccd892678824e3c06f1cc9a4ee
Nihat Osman <nihat.osman@elando.bg>
2019-11-08 10:45:34 +0200

    Implementing Lazy Loading

--- RecipeBook/src/app/app-routing.module.ts
+++ RecipeBook/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }
 ];
 
 @NgModule({
