9257007bfa0178c40ed2e35ae856bc9d1d27be5a
Nihat Osman <nihat.osman@elando.bg>
2019-11-08 11:46:07 +0200

    added Lazy loading for 'Shopping-list' and 'Auth' Components

--- RecipeBook/src/app/app.module.ts
+++ RecipeBook/src/app/app.module.ts
@@ -4,10 +4,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -15,8 +13,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule
   ],
