3488201a41f3f78c0258872b0bb95feb049b86c4
Nihat Osman <nihat.osman@elando.bg>
2019-11-12 09:16:42 +0200

    Multiple actions

--- RecipeBook/src/app/shopping-list/store/shopping-list.actions.ts
+++ RecipeBook/src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,4 +1,5 @@
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -6,3 +7,10 @@ export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
   constructor(public payload: Ingredient) {}
 }
+
+export class AddIngredients implements Action {
+  readonly type = ADD_INGREDIENTS;
+  constructor(public payload: Ingredient[]) {}
+}
+
+export type ShoppingListActions = AddIngredient | AddIngredients;
