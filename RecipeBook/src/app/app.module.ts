00d849e4ec520bcee073ddffce1153e504991779
Nihat Osman <nihat.osman@elando.bg>
2019-11-12 15:51:52 +0200

    One Root State

--- RecipeBook/src/app/app.module.ts
+++ RecipeBook/src/app/app.module.ts
@@ -7,14 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { StoreModule } from '@ngrx/store';
-import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+import * as fromApp from './store/app.reducer';
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
+    StoreModule.forRoot(fromApp.appReducer),
     SharedModule,
     CoreModule
   ],
